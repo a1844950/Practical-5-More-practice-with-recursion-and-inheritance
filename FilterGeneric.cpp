f179ee3fc7f2e7c1aa0564b2c0d890037a55def7
a1844950 <100755253+a1844950@users.noreply.github.com>
2022-09-20 19:09:04 +0800

    Update FilterGeneric.cpp

--- FilterGeneric.cpp
+++ FilterGeneric.cpp
@@ -1,11 +1,16 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <numeric>
-using namespace std;
-
 #include "FilterGeneric.h"
 
-void FilterGeneric::recursiveFilter(
-  
-)
+vector<int> FilterGeneric::fillter(vector<int> arr)
+{
+	if (arr.size() == 0) {
+		return vector<int>();
+	}
+	int curr = arr[0]; 
+	arr.erase(arr.begin()); 
+	vector<int> res = fillter(arr);
+	if (g(curr)) {
+		res.insert(res.begin(),curr);
+	}
+	return res;
+
+}
